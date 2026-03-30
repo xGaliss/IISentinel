3c2747fd34adee399ccae70b5769d622c205e8d9
Alejandro Galisteo <galisteo.alejandro00@gmail.com>
2026-03-31 01:41:10 +0200

    add Panel

---
+++ IISentinel.Panel/Models/AppPoolDto.cs
@@ -0,0 +1,7 @@
+﻿namespace IISentinel.Panel.Models;
+
+public sealed class AppPoolDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+}
---
+++ IISentinel.Panel/Program.cs
@@ -0,0 +1,32 @@
+using IISentinel.Panel.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient("Agent", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["Agent:BaseUrl"]!);
+    client.DefaultRequestHeaders.Add("x-api-key", builder.Configuration["Agent:ApiKey"]!);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
