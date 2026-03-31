48c4dc7e31250e338b96c906f80fee064da1733f
Alejandro Galisteo <galisteo.alejandro00@gmail.com>
2026-03-31 03:19:44 +0200

    add JS

---
+++ IISentinel.Panel/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿window.logsAutoScroll = () => {
+    const el = document.getElementById("logsConsole");
+    if (!el) return;
+
+    const isNearBottom = el.scrollHeight - el.scrollTop - el.clientHeight < 50;
+
+    // 🔥 comportamiento inteligente
+    if (isNearBottom) {
+        el.scrollTop = el.scrollHeight; // sigue abajo
+    }
+};
