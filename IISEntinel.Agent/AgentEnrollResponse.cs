4a6740346c3e23079373f51e9140179ef6e66266
Alejandro Galisteo <galisteo.alejandro00@gmail.com>
2026-03-31 16:26:21 +0200

    v0.2.1

--- IISEntinel.Agent/AgentEnrollResponse.cs
+++ IISEntinel.Agent/AgentEnrollResponse.cs
@@ -1,7 +1,11 @@
 ﻿namespace IISEntinel.Agent;
+using System.Text.Json.Serialization;
 
 public class AgentEnrollResponse
 {
+    [JsonPropertyName("agentId")]
     public Guid AgentId { get; set; }
+
+    [JsonPropertyName("status")]
     public string Status { get; set; } = "";
 }
